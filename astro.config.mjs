caaca2dbce99ed9eff48cf3e5830afb85ca14dbb
LIN <404@reach.com>
2025-06-05 14:43:52 +0800

    refactor: 重置项目

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +11,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon()]
 });
