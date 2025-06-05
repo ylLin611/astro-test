19808bc90f0da9085218fc88fc61afca01179875
LIN <404@reach.com>
2025-06-05 14:31:06 +0800

    build: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  base: 'astro-test'
+  base: 'astro-test',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
